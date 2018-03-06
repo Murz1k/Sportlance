ebaa0c53522b6fdd3622cb20f46f78d23838f738
Zakharov Maksim <Max89701@gmail.com>
2018-03-06 22:01:42 +0300

    add some header

--- Sportlance.WebUI/src/app/app.module.ts
+++ Sportlance.WebUI/src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
-
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import { RootComponent } from './components/root/root.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RootComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{ path: '', component: RootComponent}]),
+    MatToolbarModule,
+    FormsModule,
+    AutoCompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ Sportlance.WebUI/src/app/components/header/header.component.ts
@@ -0,0 +1,46 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  country: string;
+
+  countries: string[] = ['Russia', 'USA', 'France'];
+
+  filteredCountriesSingle: any[];
+
+  constructor(private router: Router) {
+  }
+
+  filterCountrySingle(event) {
+    let query = event.query;
+    // this.countryService.getCountries().then(countries => {
+    this.filteredCountriesSingle = this.filterCountry(query, this.countries);
+    // });
+  }
+
+  filterCountry(query, countries: any[]): any[] {
+    //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
+    let filtered: any[] = [];
+    for (let i = 0; i < countries.length; i++) {
+      let country = countries[i];
+      if (country.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(country);
+      }
+    }
+    return filtered;
+  }
+
+  async loginAsync() {
+    await this.router.navigate(['/login']);
+  }
+
+  async registerAsync() {
+    await this.router.navigate(['/register']);
+  }
+}
