b5a7fca659ccee418b1e77f7019359920af96c11
Zakharov Maksim <Max89701@gmail.com>
2018-03-07 17:19:14 +0300

    header

--- Sportlance.WebUI/src/app/components/header/header.component.ts
+++ Sportlance.WebUI/src/app/components/header/header.component.ts
@@ -29,7 +29,7 @@ export class HeaderComponent {
     let filtered: any[] = [];
     for (let i = 0; i < countries.length; i++) {
       let country = countries[i];
-      if (country.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+      if (country.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
         filtered.push(country);
       }
     }
