fcd286359f20f7989eb63f11bf746ef50cc6210c
Zakharov Maksim <Max89701@gmail.com>
2018-07-21 11:00:50 +0300

    add phone-number directory

--- Sportlance.WebUI/src/app/core/paths.ts
+++ Sportlance.WebUI/src/app/core/paths.ts
@@ -7,7 +7,7 @@ export class Paths {
   public static readonly SignUp = 'signup';
   public static readonly Login = 'login';
   public static readonly Settings = 'settings';
-  public static readonly CreateTeam = 'teams/create';
+  public static readonly CreateTeam = 'settings/my-teams/create';
   public static readonly Initialization = 'initialization';
   public static readonly ConfirmRegistration = 'auth/confirm/:id/:token';
 }
--- Sportlance.WebUI/src/app/settings/settings-routing.module.ts
+++ Sportlance.WebUI/src/app/settings/settings-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {SettingsComponent} from './settings.component';
+import {CreateTeamComponent} from './create-team/create-team.component';
 
 const routes: Routes = [
-  {path: '', component: SettingsComponent}
+  {path: '', component: SettingsComponent},
+  {path: 'my-teams/create', component: CreateTeamComponent}
 ];
 
 @NgModule({
