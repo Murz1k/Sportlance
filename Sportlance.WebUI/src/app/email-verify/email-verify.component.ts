624bfe52a62175ede63564af7d33d0450d3c4769
Zakharov Maksim <Max89701@gmail.com>
2018-12-30 14:26:18 +0300

    fix email confirm

--- Sportlance.WebUI/src/app/email-verify/email-verify.component.ts
+++ Sportlance.WebUI/src/app/email-verify/email-verify.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../auth/auth.service';
+import {tap} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'sl-email-verify',
@@ -8,17 +10,48 @@ import {AuthService} from '../auth/auth.service';
 })
 export class EmailVerifyComponent implements OnInit {
 
+  private secondsKey = 'resendEmailCooldown';
   public email: string;
+  public seconds: number;
+  public lastTime: string;
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private router: Router,
+    private authService: AuthService) {
   }
 
   ngOnInit() {
+    if (!this.authService.isAuthorized) {
+      this.router.navigate(['']);
+      return;
+    }
     this.email = this.authService.getCurrent().email;
+
+    const cooldown = localStorage.getItem(this.secondsKey);
+    if (cooldown != undefined) {
+      this.seconds = +cooldown;
+    }
+    setInterval(() => this.updateSeconds(), 1000);
+  }
+
+  private updateSeconds() {
+    if (this.seconds > 0) {
+      this.seconds--;
+      this.lastTime = new Date(null, null, null, null, null, this.seconds).toTimeString()
+        .match(/\d{2}:\d{2}:\d{2}/)[0].slice(3);
+      localStorage.setItem(this.secondsKey, this.seconds.toString());
+    }
   }
 
   resendEmail() {
-    this.authService.reSendEmail(this.authService.accessToken).subscribe();
+    this.authService.reSendEmail(this.authService.accessToken)
+      .pipe(tap((response: any) => {
+        if (!response || !response.error) {
+          this.seconds = 300;
+          localStorage.setItem(this.secondsKey, this.seconds.toString());
+        }
+      }))
+      .subscribe();
   }
 
   changeEmail(): void {
--- Sportlance.WebUI/src/app/email-verify/email-verify.module.ts
+++ Sportlance.WebUI/src/app/email-verify/email-verify.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from '@angular/common';
 import {EmailVerifyComponent} from './email-verify.component';
 import {HeaderModule} from '../core/header/header.module';
 import {EmailVerifyRoutingModule} from './email-verify-routing.module';
+import {FooterModule} from "../core/footer/footer.module";
 
 @NgModule({
   imports: [
     CommonModule,
     HeaderModule,
+    FooterModule,
     EmailVerifyRoutingModule
   ],
   declarations: [
