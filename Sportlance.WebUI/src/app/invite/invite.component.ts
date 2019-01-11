21f88691fcd867d948190e8f455259b8d672dba4
Zakharov Maksim <Max89701@gmail.com>
2019-01-11 07:49:12 +0300

    добавлена форма регистрации в Invite

--- Sportlance.WebUI/src/app/invite/invite.component.ts
+++ Sportlance.WebUI/src/app/invite/invite.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 import {AuthService} from "../core/auth/auth.service";
 import {tap} from "rxjs/operators";
 import {Title} from "@angular/platform-browser";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {RegistrationRequest} from "../core/auth/requests/registration-request";
 
 @Component({
   selector: 'sl-invite',
@@ -15,7 +17,17 @@ export class InviteComponent implements OnInit {
 
   selectedSlide: number = 0;
 
-  constructor(private titleService: Title,
+  public emailAlreadyExist: boolean;
+
+  public showTermsError: boolean;
+
+  public isLoading = false;
+
+  public form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private titleService: Title,
+              private router: Router,
               private authService: AuthService,
               private activatedRoute: ActivatedRoute) {
   }
@@ -24,6 +36,14 @@ export class InviteComponent implements OnInit {
     this.titleService.setTitle(`Sportlance делает жизнь проще - рекомендую`);
 
     this.userName = this.activatedRoute.snapshot.data['user'].firstName;
+
+    this.form = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.maxLength(20)]],
+      lastName: ['', [Validators.required, Validators.maxLength(30)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      isAcceptTerms: [false]
+    });
   }
 
   preview() {
@@ -32,5 +52,39 @@ export class InviteComponent implements OnInit {
 
   next() {
     this.selectedSlide++;
+  }
+
+  signUp(): void {
+    if (!this.form.controls['isAcceptTerms'].value) {
+      this.showTermsError = true;
+      return;
+    }
+
+    if (this.form.invalid) {
+      return;
+    }
+    const form = this.form.value;
+
+    this.isLoading = true;
+
+    this.authService.register(<RegistrationRequest>{
+      email: form.email,
+      lastName: form.lastName,
+      password: form.password,
+      firstName: form.firstName
+    }).pipe(tap((response) => {
+      if (!response.error) {
+        this.authService.saveTokens(response);
+        this.router.navigate(['email-verify']);
+      } else {
+        this.isLoading = false;
+      }
+    })).subscribe();
+  }
+
+  public scrollToForm(): void {
+    const offsetTop1 = document.getElementById('form').offsetTop;
+    window.scrollTo({left: 0, top: offsetTop1 - 40, behavior: 'smooth'});
+    return;
   }
 }
--- Sportlance.WebUI/src/app/invite/invite.module.ts
+++ Sportlance.WebUI/src/app/invite/invite.module.ts
@@ -4,6 +4,10 @@ import {RouterModule, Routes} from "@angular/router";
 import {InviteComponent} from "./invite.component";
 import {RedirectInviteResolver} from "./redirect-invite.resolver";
 import {SlButtonModule} from "../shared/button/button.module";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatCheckboxModule} from "@angular/material";
+import {SlInputModule} from "../shared/input/input.module";
+import {FooterModule} from "../core/footer/footer.module";
 
 const routes: Routes = [
   {path: ':link', component: InviteComponent, resolve: {user: RedirectInviteResolver}}
@@ -13,7 +17,11 @@ const routes: Routes = [
   declarations: [InviteComponent],
   imports: [
     RouterModule.forChild(routes),
+    MatCheckboxModule,
+    ReactiveFormsModule,
     SlButtonModule,
+    SlInputModule,
+    FooterModule,
     CommonModule
   ],
   providers: [RedirectInviteResolver]
