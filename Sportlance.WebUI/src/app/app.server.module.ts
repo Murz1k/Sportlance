dac4118ca8d89c737e4fb290c2bd27a7a1e2fb45
Zakharov Maksim <Max89701@gmail.com>
2019-01-31 07:37:38 +0300

    fix build

--- Sportlance.WebUI/src/app/app.server.module.ts
+++ Sportlance.WebUI/src/app/app.server.module.ts
@@ -2,23 +2,12 @@ import {NgModule} from '@angular/core';
 import {ServerModule} from '@angular/platform-server';
 
 import {AppModule} from "./app.module";
-import {AppComponent} from "./app.component";
-import {RootComponent} from "./root/root.component";
-import {LandingComponent} from "./landing/landing.component";
-import {AboutComponent} from "./about/about.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RootComponent,
-    LandingComponent,
-    AboutComponent
-  ],
   imports: [
     AppModule,
     ServerModule
-  ],
-  bootstrap: [AppComponent]
+  ]
 })
 export class AppServerModule {
 }
